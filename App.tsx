8f173d1d037946c238b9ebf7125b45f1635c0453
Juan Sebastián Pulidori <101568805+juansepulidori@users.noreply.github.com>
2024-05-16 18:32:05 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView, WebViewMessageEvent } from "react-native-webview";
+import Constants from "expo-constants";
+import { Button, Platform, StyleSheet, TextInput, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { PermissionsAndroid } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const webViewRef = useRef<WebView>(null);
+
+    const [url, setUrl] = useState<string>("");
+    const [search, setSearch] = useState<Boolean>(false);
+    const message = "Hola vengo desde la app nativa";
+
+    useEffect(() => {
+        requestCameraPermission();
+    });
+
+    const requestCameraPermission = async () => {
+        if (Platform.OS === "android") {
+            try {
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.CAMERA,
+                    {
+                        title: "Permiso de cámara",
+                        message: "Esta aplicación necesita acceder a tu cámara.",
+                        buttonNeutral: "Preguntar luego",
+                        buttonNegative: "Cancelar",
+                        buttonPositive: "Aceptar",
+                    }
+                );
+                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (err) {
+                console.warn(err);
+                return false;
+            }
+        }
+    };
+
+    const handleFunction = (event: WebViewMessageEvent) => {
+        switch (event.nativeEvent.data) {
+            case "requestCameraPermission":
+                requestCameraPermission();
+                break;
+            case "requesrMessage":
+                sendMessage();
+                break;
+            default:
+                break;
+        }
+    };
+
+    const press = () => {
+        setSearch(!search);
+    };
+
+    const sendMessage = () => {
+        const jsCode = `readMessage('${message}');`;
+        console.log(jsCode)
+        webViewRef.current?.injectJavaScript(jsCode);
+    };
+
+    return (
+        <View style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
+            {search ? (
+                <>
+                    <WebView
+                        ref={webViewRef}
+                        source={{
+                            uri: url,
+                        }}
+                        onMessage={handleFunction}
+                    />
+                    <Button title="Volver" onPress={press}></Button>
+                </>
+            ) : (
+                <View style={styles.container}>
+                    <TextInput
+                        style={styles.textInput}
+                        onChangeText={(text) => setUrl(text)}
+                    ></TextInput>
+                    <Button title="Buscar" onPress={press}></Button>
+                </View>
+            )}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textInput: {
+        width: "80%",
+        borderColor: "black",
+        borderWidth: 1,
+        borderRadius: 30,
+        marginBottom: 10,
+    },
 });
---
+++ notas.md
@@ -0,0 +1,12 @@
+| Tarea                  | Completada |
+| ---------------------- | ---------- |
+| Sacar Foto             |            |
+| Escanear QR            |            |
+| Devolver foto a web    |            |
+| Probar imputs WebView  |            |
+| Probar botones WebView |            |
+
+
+Documentación Comunicación entre nativo y webview
+https://github.com/react-native-webview/react-native-webview/blob/4e1ede4ba3cfc770f9d4a2485c4850c4f3f1a708/docs/Guide.md#communicating-between-js-and-native
+
